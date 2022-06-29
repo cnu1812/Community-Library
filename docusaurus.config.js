14634a5f3e2c25a61d70cdc8147437d78e0e8964
cnu1812 <cnu1812@gmail.com>
2022-06-29 19:49:17 +0530

    links update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/cnu1812/community-library',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/cnu1812/community-library',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
