ebb806944c81dfa396f3fa652f7a75c740d6bd47
cnu1812 <cnu1812@gmail.com>
2022-06-30 12:21:31 +0530

    Spell checks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Documentation',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
@@ -72,7 +72,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             title: 'Docs',
             items: [
               {
-                label: 'Documentation',
+                label: 'Docs',
                 to: '/docs/intro',
               },
             ],
