8135ed0dd157bc1d7b7ba99d315160c26ed46b32
Sreenathkb19 <104550058+Sreenathkb19@users.noreply.github.com>
2022-11-25 13:26:41 +0530

    doc

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,7 @@
 module.exports = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   mainSidebar: [
-    'PowerStackies', 'Leaderboard', 'intro',
+    'PowerStackies', 'Leaderboard', 'CommunityCollabDiscussion', 'intro',
     {
       type: 'category',
       label: '30 Days of Web2',
